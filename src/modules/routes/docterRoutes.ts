fb8fdcfe201a6a2c362debe25a257cd375f90f8e
Awais ali <awaisali788788@gmail.com>
2023-05-18 21:30:08 +0500

    add new docter and get single docter function implimentation

--- src/modules/routes/docterRoutes.ts
+++ src/modules/routes/docterRoutes.ts
@@ -1,7 +1,8 @@
 import express, { Express } from "express";
 import docterControler from "../../controlers/docter/docterControler";
 const docterRoutes=express();
- docterRoutes.use("/",docterControler.docter)
+docterRoutes.post("/",docterControler.addDocter)
+docterRoutes.get("/:id?",docterControler.getDocter)
 
 
 export default docterRoutes
